1e6f423e88fec833af679c55d04b887787f9f1ec
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-10-08 09:58:09 +0200

    Usa funciones para las operaciones y cambio de los operadores ternario por if-elseif, por simplicidad y porque tampoco funcionaban bien

--- formularios/calculadora/calculadora.php
+++ formularios/calculadora/calculadora.php
@@ -15,6 +15,13 @@
 
                 return $data;
             } // No se usa en este ejercicio porque el formulario no usa campos de texto
+
+
+            // Usaría funciones flecha, pero la versión de PHP del servidor es más antigua 
+            function suma($a, $b) { return $a + $b; }
+            function resta($a, $b) { return $a - $b; }
+            function prod($a, $b) { return $a * $b; }
+            function div($a, $b) { return ($b != 0) ? $a / $b : "indefinido"; }
             
             echo "<h1>CALCULADORA</h1>";
 
@@ -29,14 +36,12 @@
             $op1 = $_POST['op1'];
             $op2 = $_POST['op2'];
 
-            $resultado =
-                ($operacion == "suma") ? $op1 + $op2 :
-                ($operacion == "resta") ? $op1 - $op2 :
-                ($operacion == "prod") ? $op1 * $op2 :
-                ($operacion == "div" && $op2 != 0) ? $op1 / $op2 :
-                "indefinido";
+            if ($operacion == "suma") $resultado = suma($op1, $op2);
+            elseif ($operacion == "resta") $resultado = resta($op1, $op2);
+            elseif ($operacion == "prod") $resultado = prod($op1, $op2);
+            elseif ($operacion == "div") $resultado = div($op1, $op2);
 
-            echo "$op1 {$operadores[$operacion]} $op2 = $resultado";
+            if (isset($operadores[$operacion])) echo "$op1 {$operadores[$operacion]} $op2 = $resultado";
         ?>
     </body>
 </html>
